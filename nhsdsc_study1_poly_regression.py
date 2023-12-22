da88e2a5083d889c28654794a610244509e352f7
Tim Zhao <timzhao938@outlook.com>
2023-12-22 23:32:15 +0800

    Adjusted factors.

--- nhsdsc_study1_poly_regression.py
+++ nhsdsc_study1_poly_regression.py
@@ -7,12 +7,12 @@ dtst_dir = "datasets\diabetes_012_health_indicators_BRFSS2015.csv"
 
 dtst = pandas.read_csv(dtst_dir)
 
-x = dtst[["GenHlth"]]
+x = dtst[["GenHlth", "HighBP", "HighChol", "CholCheck", "BMI", "Smoker", "Stroke", "HeartDiseaseorAttack", "PhysActivity", "Fruits", "Veggies", "HvyAlcoholConsump", "AnyHealthcare", "NoDocbcCost", "GenHlth", "PhysHlth", "DiffWalk", "Sex", "Age", "Education", "Income"]]
 y = dtst["MentHlth"]
 
 print(x)
 
-x = PolynomialFeatures(degree = 5).fit_transform(x) # Changing degree into 1 results in linear regression model.
+x = PolynomialFeatures(degree = 3).fit_transform(x) # Changing degree into 1 results in linear regression model.
 model = linear_model.LinearRegression()
 model.fit(x, y)
 
